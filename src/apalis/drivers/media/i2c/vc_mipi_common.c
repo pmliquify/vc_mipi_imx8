32376b3916c4027c44b9cc5ff8c4717b2148c018
Peter Martienssen <peter.martienssen@liquify-consulting.de>
2021-04-02 18:07:58 +0200

    Commit after mysterios merge conflict

--- src/apalis/drivers/media/i2c/vc_mipi_common.c
+++
@@ -1,9 +0,0 @@
-
-#include "vc_mipi_common.h"
-#include "vc_mipi_ctrl.h"
-
-#include <linux/device.h>
-#include <linux/i2c.h>
-
-#define TRACE printk("        TRACE [vc-mipi] vc_mipi_common.c --->  %s : %d", __FUNCTION__, __LINE__);
-//#define TRACE
--- src/apalis/drivers/media/i2c/vc_mipi_common.h
+++
@@ -1,8 +0,0 @@
-#ifndef _VC_MIPI_COMMON_H
-#define _VC_MIPI_COMMON_H
-
-#include "imx226.h"
-
-
-
-#endif // _VC_MIPI_COMMON_H
